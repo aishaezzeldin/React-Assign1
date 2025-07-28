d049a3392c7526cedb90ffcd461f1a598f774b3f
aishaezzeldin <aishaashraf1000@gmail.com>
2025-07-28 21:51:30 +0300

    Second  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
-// import './App.css';
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import About from './assets/Components/About/About';
-// import Contact from './assets/Components/Contact/Contact';
-// import Portfolio from './assets/Components/Portfolio/Portfolio'; 
-// import Home from './assets/Components/Home/Home'; 
-
-
-// const Router = createBrowserRouter([
- 
-//   { path: '*', element:<h5>404</h5> },
-//   { path: 'Home', element: <Home />,  children:[
-//   { path: 'About', element: <About /> },
-//   { path: 'Contact', element: <Contact /> },
-//   { path: 'Portfolio', element: <Portfolio /> },
-//   ]
-// }
- 
-// ])
-// App.jsx
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {  createHashRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./assets/Components/Home/Home";
 import About from "./assets/Components/About/About";
 import Portfolio from "./assets/Components/Portfolio/Portfolio";
 import Contact from "./assets/Components/Contact/Contact";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Layout/>,
